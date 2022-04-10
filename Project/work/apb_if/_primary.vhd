27cfcdedacfae0c49f7a70ee850ccab034997dfc
michaelarabian <michael-nareg99@hotmail.com>
2022-04-10 17:44:27 -0400

    added top

--- Project/work/apb_if/_primary.vhd
+++
@@ -1,7 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity apb_if is
-    port(
-        PClk            : in     vl_logic
-    );
-end apb_if;
--- Project/work/apb_result_sv_unit/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity apb_result_sv_unit is
-end apb_result_sv_unit;
